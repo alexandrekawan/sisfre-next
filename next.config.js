0f4fc6493cca46443437dc746044226f58dfb4ec
Alexandre Kawan <alexandrekawan@outlook.com>
2022-06-03 17:17:08 -0300

    FIX 2

--- next.config.js
+++ next.config.js
@@ -1,15 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  redirects: async function () {
-    return [
-      {
-        source: '/',
-        destination: '/login',
-        permanent: true
-      }
-    ];
-  }
+  reactStrictMode: true
 }
 
 module.exports = nextConfig
