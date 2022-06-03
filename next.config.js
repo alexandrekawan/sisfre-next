6b2f8baeede357d83415a2570949b716a52623de
Alexandre Kawan <alexandrekawan@outlook.com>
2022-06-03 17:15:53 -0300

    FIX

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  redirects: async function () {
+    return [
+      {
+        source: '/',
+        destination: '/login',
+        permanent: true
+      }
+    ];
+  }
 }
 
 module.exports = nextConfig
--- src/pages/login.js
+++ src/pages/login.js
@@ -1,8 +1,13 @@
+import Head from 'next/head'
 import { Input, Button, Flex, Container } from '@chakra-ui/react'
 
 function Login() {
     return (
         <Flex w='100vw' h='100vh' align='center' justify='center'>
+            <Head>
+                <title>SisFRE</title>
+            </Head>
+
             <Container border='1px' borderStyle='solid' borderColor='black' borderRadius='md'>
                 <form method="POST" action="#">
                     <Input type="email" required/>
